e3ce6d5074c119681697c799b46a78a4256beddf
Ankush Mukherjee <ankush.mukherjee24@gmail.com>
2023-04-15 14:02:05 +0530

    add validation for image upload

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -6,6 +6,22 @@ const fs = require("fs");
 // Create a product
 exports.createProduct = async (req, res) => {
   try {
+    if (req.files || req.files.image) {
+      // Array of allowed files
+      const array_of_allowed_files = ["png", "jpeg", "jpg"];
+
+      // Get the extension of the uploaded file
+      const file_extension = req.files.image.name.split(".")[1];
+     
+      // Check if the uploaded file is allowed
+      if (!array_of_allowed_files.includes(file_extension)) {
+        return res.status(400).json({
+          success: false,
+          message: "Please upload jpeg, jpg or png only",
+        });
+      }
+    }
+
     let product = await Product.create({
       productName: req.body.productName,
       description: req.body.description,
@@ -76,7 +92,7 @@ exports.getProductDetials = async (req, res) => {
 exports.updateProduct = async (req, res) => {
   try {
     let product = await Product.findById(req.params.id);
-    console.log(product)
+    console.log(product);
     let imagePath;
 
     if (!product) {
@@ -86,28 +102,46 @@ exports.updateProduct = async (req, res) => {
       });
     }
 
-    if (req.files && req.files.image){
+    if (req.files || req.files.image) {
+      // Array of allowed files
+      const array_of_allowed_files = ["png", "jpeg", "jpg"];
+
+      // Get the extension of the uploaded file
+      const file_extension = req.files.image.name.split(".")[1];
+     
+      // Check if the uploaded file is allowed
+      if (!array_of_allowed_files.includes(file_extension)) {
+        return res.status(400).json({
+          success: false,
+          message: "Please upload jpeg, jpg or png only",
+        });
+      }
+
       req.files.image.mv("uploads/" + product._id + ".jpg");
-      imagePath = req.protocol + "://" + req.get("host") + "/uploads/" +  product._id + ".jpg";
+      imagePath = req.protocol + "://" + req.get("host") + "/uploads/" + product._id + ".jpg";
     }
 
+   
     // product.productName = req.body.productName;
     // product.description = req.body.description;
     // product.price = Number.parseFloat(req.body.price);
     // product.image =  imagePath != "" ? imagePath : "";
 
     // await product.save();
-    let updateProduct = await Product.findByIdAndUpdate(req.params.id,{
-        productName : req.body.productName,
-        description : req.body.description,
-        price :Number.parseFloat(req.body.price),
-        image : imagePath != "" ? imagePath : "",
-    },{new:true})
+    let updateProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        productName: req.body.productName,
+        description: req.body.description,
+        price: Number.parseFloat(req.body.price),
+        image: imagePath != "" ? imagePath : "",
+      },
+      { new: true }
+    );
 
     res.status(201).json({
       success: true,
-      updateProduct
-     
+      updateProduct,
     });
   } catch (err) {
     console.log(err.message);
@@ -119,7 +153,6 @@ exports.deleteProduct = async (req, res) => {
   try {
     //console.log(req.params.id)
     const product = await Product.findById(req.params.id);
-   
 
     if (!product) {
       return res.status(404).json({
